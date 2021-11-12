acea4cba8cbf1bd51d92546a6348de3a35075d53
Mario55770 <feretty@outlook.com>
2021-11-12 13:54:56 -0500

    Add project files.

---
+++ 1.3/Defs/TrapDefs/TrapDef.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace DShield
+{
+    class TrapDef: DefModExtension
+    {
+        public bool targetLegs=false; 
+        public DamageDef damageType;
+       
+    }
+}
